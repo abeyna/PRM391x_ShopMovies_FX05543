d6945e6c28864729c10268379cdce9095fbf7eb6
Luan Nguyen <67037684+abeyna@users.noreply.github.com>
2021-05-18 16:12:00 +0700

    Initial commit

---
+++ app/src/main/java/funix/assignment/prm391x_shopmovies_fx05543/MainActivity.java
@@ -0,0 +1,14 @@
+package funix.assignment.prm391x_shopmovies_fx05543;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
