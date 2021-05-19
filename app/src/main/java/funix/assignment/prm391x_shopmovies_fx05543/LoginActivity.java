775143f3697af1fd22f7b0e3830d069033c22d2a
Luan Nguyen <67037684+abeyna@users.noreply.github.com>
2021-05-19 17:55:51 +0700

    Create login buttons for layout and LoginActivity.

---
+++ app/src/main/java/funix/assignment/prm391x_shopmovies_fx05543/LoginActivity.java
@@ -0,0 +1,12 @@
+package funix.assignment.prm391x_shopmovies_fx05543;
+
+import android.support.v7.app.AppCompatActivity;
+
+import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.common.SignInButton;
+
+public class LoginActivity extends AppCompatActivity {
+    private LoginButton mFbBtn;
+    private SignInButton mGgBtn;
+
+}
--- app/src/main/java/funix/assignment/prm391x_shopmovies_fx05543/MainActivity.java
+++ app/src/main/java/funix/assignment/prm391x_shopmovies_fx05543/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
