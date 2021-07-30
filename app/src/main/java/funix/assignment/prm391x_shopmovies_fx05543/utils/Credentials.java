a4daaca3a5e8c87ebfd9a51a30e37a155df5716f
Luan Nguyen <nguyenngocluan.298@gmail.com>
2021-07-30 21:38:51 +0700

    Option 2.0

--- app/src/main/java/funix/assignment/prm391x_shopmovies_fx05543/utils/Credentials.java
+++
@@ -1,7 +0,0 @@
-package funix.assignment.prm391x_shopmovies_fx05543.utils;
-
-public class Credentials {
-    public static final String BASE_URL = "https://api.themoviedb.org/";
-
-    public static final String API_KEY = "270a8071656dfc411abbdf3c0e310219";
-}
