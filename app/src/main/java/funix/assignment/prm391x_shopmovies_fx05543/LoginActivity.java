acff8f3e39f83af5ff7657924bb2be45ca91653f
Luan Nguyen <nguyenngocluan.298@gmail.com>
2021-07-31 15:54:35 +0700

    Option 1: Facebook login button

--- app/src/main/java/funix/assignment/prm391x_shopmovies_fx05543/LoginActivity.java
+++ app/src/main/java/funix/assignment/prm391x_shopmovies_fx05543/LoginActivity.java
@@ -2,12 +2,25 @@ package funix.assignment.prm391x_shopmovies_fx05543;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.common.SignInButton;
+
+import org.json.JSONObject;
+
+import java.util.Arrays;
 
 public class LoginActivity extends AppCompatActivity {
     private LoginButton mFbBtn;
@@ -15,17 +28,56 @@ public class LoginActivity extends AppCompatActivity {
     private CallbackManager mCallbackManager;
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        setContentView(R.layout.activity_login);
+
+        mCallbackManager = CallbackManager.Factory.create();
+        mFbBtn = findViewById(R.id.activity_login_fb_login_btn);
+        mGgBtn = findViewById(R.id.activity_login_gg_login_btn);
+
+        mFbBtn.setReadPermissions(Arrays.asList("public_profile", "email"));
+        loginFacebook();
+    }
+
+    private void loginFacebook() {
+        mFbBtn.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                getInformation();
+                Intent intent = new Intent(LoginActivity.this, MoviesActivity.class);
+                startActivity(intent);
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+    }
+
+    private void getInformation() {
+        GraphRequest graphRequest = GraphRequest.newMeRequest(AccessToken.getCurrentAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+            @Override
+            public void onCompleted(JSONObject object, GraphResponse response) {
+                Log.d("info_JSON", response.getJSONObject().toString());
+            }
+        });
+        Bundle parameter = new Bundle();
+        parameter.putString("fields", "name,email,first_name");
+        graphRequest.setParameters(parameter);
+        graphRequest.executeAsync();
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
-        mFbBtn = findViewById(R.id.activity_login_fb_login_btn);
-        mGgBtn = findViewById(R.id.activity_login_gg_login_btn);
-        mCallbackManager = CallbackManager.Factory.create();
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable @org.jetbrains.annotations.Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
---
+++ app/src/main/java/funix/assignment/prm391x_shopmovies_fx05543/MoviesActivity.java
@@ -0,0 +1,14 @@
+package funix.assignment.prm391x_shopmovies_fx05543;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MoviesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movies);
+    }
+}
