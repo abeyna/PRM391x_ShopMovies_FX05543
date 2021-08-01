6d098fe5b722840e377fbbcd0c8d46e56b772df8
Luan Nguyen <nguyenngocluan.298@gmail.com>
2021-08-01 08:37:24 +0700

    Option 1: Google login button

--- app/src/main/java/funix/assignment/prm391x_shopmovies_fx05543/LoginActivity.java
+++ app/src/main/java/funix/assignment/prm391x_shopmovies_fx05543/LoginActivity.java
@@ -1,8 +1,10 @@
 package funix.assignment.prm391x_shopmovies_fx05543;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,7 +18,13 @@ import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 import org.json.JSONObject;
 
@@ -26,6 +34,8 @@ public class LoginActivity extends AppCompatActivity {
     private LoginButton mFbBtn;
     private SignInButton mGgBtn;
     private CallbackManager mCallbackManager;
+    private GoogleSignInClient mGoogleSignInClient;
+    private static int RC_SIGN_IN = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,18 +44,20 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         mCallbackManager = CallbackManager.Factory.create();
-        mFbBtn = findViewById(R.id.activity_login_fb_login_btn);
-        mGgBtn = findViewById(R.id.activity_login_gg_login_btn);
+        mFbBtn = findViewById(R.id.activity_login_fb_btn);
+        mGgBtn = findViewById(R.id.activity_login_gg_btn);
 
         mFbBtn.setReadPermissions(Arrays.asList("public_profile", "email"));
+
         loginFacebook();
+        loginGoogle();
     }
 
     private void loginFacebook() {
         mFbBtn.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-                getInformation();
+                getFBInformation();
                 Intent intent = new Intent(LoginActivity.this, MoviesActivity.class);
                 startActivity(intent);
             }
@@ -62,7 +74,7 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
-    private void getInformation() {
+    private void getFBInformation() {
         GraphRequest graphRequest = GraphRequest.newMeRequest(AccessToken.getCurrentAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
             @Override
             public void onCompleted(JSONObject object, GraphResponse response) {
@@ -75,9 +87,69 @@ public class LoginActivity extends AppCompatActivity {
         graphRequest.executeAsync();
     }
 
+    private void loginGoogle() {
+        // Configure sign-in to request the user's ID, email address, and basic profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        // Check for existing Google Sign In account, if the user is already signed in the GoogleSignInAccount will be non-null.
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+
+        // Set the dimensions of the sign-in button.
+        mGgBtn.setSize(SignInButton.SIZE_STANDARD);
+
+        mGgBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signIn();
+            }
+        });
+    }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
+            if (acct != null) {
+                String personName = acct.getDisplayName();
+                String personGivenName = acct.getGivenName();
+                String personFamilyName = acct.getFamilyName();
+                String personEmail = acct.getEmail();
+                String personId = acct.getId();
+                Uri personPhoto = acct.getPhotoUrl();
+            }
+
+            // Signed in successfully, show authenticated UI.
+            Intent intent = new Intent(LoginActivity.this, MoviesActivity.class);
+            startActivity(intent);
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.d("message", e.toString());
+        }
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable @org.jetbrains.annotations.Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+
         mCallbackManager.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
     }
 }
